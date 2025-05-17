abc617826a6e8652dac1743ab7c5727da6f15a6f
SystemNasa <100525301+SystemNasa@users.noreply.github.com>
2025-05-17 20:21:23 +0200

    Update freaky.lua

--- freaky.lua
+++ freaky.lua
@@ -1,7 +1,12 @@
 return {
-    ["Make It Wiggle"] = 113683061479694,
+    ["Wiggle(blocky avatar)"] = 113683061479694,
     ["Head"] = 118252306357834,
     ["Jerk and wave"] = 81589806749560,
     ["Ride It"] = 114298502005682,
-    ["Hump The Air"] = 89556542947425
+    ["Hump The Air"] = 89556542947425,
+    ["Twerking"] = 77493234914180,
+    ["Backshots"] = 107788763755159,
+    ["Jerk off"] = 85510928563896,
+    ["Jerk and suck"] = 138147248157775,
+    ["Dick Copter"] = 101470134688106
 }
